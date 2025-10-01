01310b2aadbfadf6e867b9e1fae39f1c2d6ccf98
IsaiasTech <isaiasbatistapy@gmail.com>
2025-10-01 12:59:34 -0300

    criando projeto firebase exemplo para autenticação

---
+++ js/firebaseExample.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "apikey",
+  authDomain: "authdomain",
+  projectId: "projectid",
+  storageBucket: "storagebucket",
+  messagingSenderId: "messagingsenderid",
+  appId: "appid"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
